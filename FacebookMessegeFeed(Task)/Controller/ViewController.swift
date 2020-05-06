547e4182b8a26f212b6d6c3889ac75c473432639
Md Taj Hassan <tajhassan0355@gmail.com>
2020-05-07 00:40:34 +0530

    facebookfeedTask

--- FacebookMessegeFeed(Task)/Controller/ViewController.swift
+++ FacebookMessegeFeed(Task)/Controller/ViewController.swift
@@ -20,6 +20,7 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        print("gdfh")
         
         getDataFromServer()
  
